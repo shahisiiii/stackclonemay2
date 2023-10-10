9cdc0b7f0780ea8cb2582de9bfbe402ab8b743a8
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-10-10 22:23:25 +0530

    third commit

--- stackclonemay/settings.py
+++ stackclonemay/settings.py
@@ -138,3 +138,14 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+
+
+REST_FRAMEWORK = {
+    ...
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        ...
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
